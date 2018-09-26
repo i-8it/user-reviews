adf0833ff1ca530ad2f565fd2c302999068e522d
michchen <michchen58@gmail.com>
2018-09-26 15:24:17 -0700

    Add CRUD functionality

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,34 +1,6 @@
 const Restaurant = require("../models/review.js");
 // http://localhost:3002/reviews/100/
 
-const example = {
-  name: 'ice cream patio2',
-  id: 40,
-  ratings: 5,
-  reviewsCount: 488,
-  useful_count: 3,
-  funny_count: 3,
-  cool_count: 3,
-  useful_clicked: false,
-  funny_clicked: false,
-  cool_clicked: false,
-  review: {
-    count_starRatings: 2,
-    date: '2015-07-20T22:04:34.404Z',
-    text_review: 'Rem sapiente quaerat nesciunt temporibus est. Perferendis eos et provident ea. Laboriosam odit quo repellat voluptas consequatur deleniti. Neque ipsa molestiae voluptatibus rerum itaque.',
-    count_checkin: 1,
-    user: {
-      name: 'Cordia Satterfield',
-      city: 'Dorothealand',
-      state: 'DE',
-      profile_image: 'https://s3.amazonaws.com/uifaces/faces/twitter/Karimmove/128.jpg',
-      count_friends: 88,
-      count_reviews: 280,
-      count_photos: 127
-    }
-  }
-};
-
 const nameToUrl = function(name) {
   return name.replace(/ /g, '-');
 };

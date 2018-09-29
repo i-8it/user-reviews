e4ee9074965c39c6f429b786b861a2228078bc36
michchen <michchen58@gmail.com>
2018-09-29 16:39:07 -0700

    Figure out insert syntax

--- database/getReviews.js
+++ database/getReviews.js
@@ -10,8 +10,18 @@ SELECT * FROM (
 INNER JOIN (
   select * from users
 ) t2
-on t1.user_id=t2.id;
+on t1.user_id=t2.id
+ORDER BY date DESC
+;
 
 */
 
-// SELECT * FROM (SELECT * FROM reviews WHERE id IN (SELECT rev FROM jointable WHERE rest=(SELECT id FROM restaurants WHERE name=UPPER('davis''s fantastic handcrafted soft table #87')))) t1 INNER JOIN (select * from users) t2 on t1.user_id=t2.id;
+// SELECT * FROM (SELECT * FROM reviews WHERE id IN (SELECT rev FROM jointable WHERE rest=(SELECT id FROM restaurants WHERE name=UPPER('davis''s fantastic handcrafted soft table #87')))) t1 INNER JOIN (select * from users) t2 on t1.user_id=t2.id ORDER BY date DESC;
+
+
+
+///// CASSANDRA /////
+
+/*
+INSERT INTO reviews (id, restaurant_id, cool_clicked, cool_count, funny_clicked, funny_count, name, ratings, reviewscount, useful_clicked, useful_count, user) VALUES ( 10, 11, 0, 0, 1, 1, 'mario', 301, 219, 0, 2, {name: 'lorem'});
+*/

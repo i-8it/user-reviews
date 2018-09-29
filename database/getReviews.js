7b192b6d64235255b9605b904ed9e74da3f42664
michchen <michchen58@gmail.com>
2018-09-29 10:44:48 -0700

    Move files in mseed into mseed/postgres

--- database/getReviews.js
+++ database/getReviews.js
@@ -1,6 +1,17 @@
+/*
 
+SELECT * FROM (
+  SELECT * FROM reviews WHERE id IN (
+    SELECT rev FROM jointable WHERE rest=(
+      SELECT id FROM restaurants WHERE name=UPPER('Davis''s Fantastic Handcrafted Soft Table #87')
+   )
+ )
+) t1
+INNER JOIN (
+  select * from users
+) t2
+on t1.user_id=t2.id;
 
+*/
 
-// select * from reviews where id in (SELECT rev FROM jointable WHERE rest=(SELECT id FROM restaurants WHERE name='Davis''s Fantastic Handcrafted Soft Table #87'));
-// select * from reviews where id in (SELECT rev FROM jointable WHERE rest=(SELECT id FROM restaurants WHERE name='Jacobson''s Incredible Unbranded Steel Pants #96'));
-// select * from (select * from reviews where id in (SELECT rev FROM jointable WHERE rest=(SELECT id FROM restaurants WHERE name='Davis''s Fantastic Handcrafted Soft Table #87'))) t1 INNER JOIN (select * from users) t2 on t1.user_id=t2.id;
+// SELECT * FROM (SELECT * FROM reviews WHERE id IN (SELECT rev FROM jointable WHERE rest=(SELECT id FROM restaurants WHERE name=UPPER('davis''s fantastic handcrafted soft table #87')))) t1 INNER JOIN (select * from users) t2 on t1.user_id=t2.id;

8affac7bc041c8cae912b162da83cf220983eb9d
michchen <michchen58@gmail.com>
2018-10-01 18:56:57 -0700

    Refactor cassandra insert script to use cassandra-driver Batch

--- mseed/cassandra/insert.js
+++ mseed/cassandra/insert.js
@@ -5,133 +5,83 @@ const client = new cassandra.Client({
   keyspace: 'yelp'
 });
 
+fs.readFile('../data/reviewsWithRating/01.csv', (err, data) => {
+  let query = String(data);
 
-// read from data/reviews
+  query = query.substr(0, 3000);
+  query = query.substr(0, query.lastIndexOf('\n'));
+// console.log(query);
+  // construct json objects w/in user and reviews
 
-// CASSANDRA SCHEMA
-
-// id
-// restaurant_id
-// cool_clicked
-// cool_count
-// funny_clicked
-// funny_count
-// name
-// ratings
-// review
-// reviewscount
-// useful_clicked
-// useful_count
+// --DATABASE
+// id,
+// cool_clicked,
+// cool_count,
+// funny_clicked,
+// funny_count,
+// name,
+// ratings,
+// review,
+// reviewscount,
+// useful_clicked,
+// useful_count,
 // user
 
+// JESSE REV OBJECT
+// "count_starRatings":2,
+// "date":"2015-07-20T22:04:34.404Z",
+// "text_review":"Rem sapiente quaerat nesciunt temporibus est. Perferendis eos et provident ea. Laboriosam odit quo repellat voluptas consequatur deleniti. Neque ipsa molestiae voluptatibus rerum itaque.",
+// "count_checkin":1,
 
-// JESSE DATA
+// JESSE USER OBJECT
+// id: 0,
+// "name":"Fanny Hilpert",
+// "city":"South Charlieburgh",
+// "state":"KY",
+// "profile_image":"https://s3.amazonaws.com/uifaces/faces/twitter/iamkeithmason/128.jpg",
+// "count_friends":630,
+// "count_reviews":389,
+// "count_photos":107
 
+// --CSV
+// 0  id??? maybe? // ${((fileNum - 1) * chunkSize + restNum)},\????
+// 1  useful_count
+// 2  funny_count
+// 3  cool_count
+// 4  useful_clicked
+// 5  funny_clicked
+// 6  cool_clicked
+// 7  date
+// 8  review text
+// 9  checkins
+// 10 user number
+// 11 rating
 
-// "name":"ice cream patio",
-// "id":40,
-// "ratings":5,
-// "reviewsCount":488,
-// "useful_count":3,
-// "funny_count":3,
-// "cool_count":3,
-// "useful_clicked":false,
-// "funny_clicked":false,
-// "cool_clicked":false,
-// "review":{
-//   "count_starRatings":2,
-//   "date":"2015-07-20T22:04:34.404Z",
-//   "text_review":"Rem sapiente quaerat nesciunt temporibus est. Perferendis eos et provident ea. Laboriosam odit quo repellat voluptas consequatur deleniti. Neque ipsa molestiae voluptatibus rerum itaque.",
-//   "count_checkin":1,
-//   "user":{
-//     "name":"Cordia Satterfield",
-//     "city":"Dorothealand",
-//     "state":"DE",
-//     "profile_image":"https://s3.amazonaws.com/uifaces/faces/twitter/Karimmove/128.jpg",
-//     "count_friends":88,
-//     "count_reviews":280,
-//     "count_photos":127
-//   }
-// }
-/*
+  query = query.split('\n').map((elt) => {
+    let p = elt.split(',');
+    let review = {
+      count_starRatings: p[11],
+      date: p[7],
+      text_review: p[8],
+      count_checkin: p[9]
+    };
+    let user = {
+      id: p[10]
+    }
 
-// my data: data/reviews
-{
-  count_starRatings:2,
-  date:"2015-07-20T22:04:34.404Z",
-  text_review:"Rem sapiente quaerat nesciunt temporibus est. Perferendis eos et provident ea. Laboriosam odit quo repellat voluptas consequatur deleniti. Neque ipsa molestiae voluptatibus rerum itaque.",
-  count_checkin:1,
-  user:{
-    name:"Cordia Satterfield",
-    city:"Dorothealand",
-    state:"DE",
-    profile_image:"https://s3.amazonaws.com/uifaces/faces/twitter/Karimmove/128.jpg",
-    count_friends:88,
-    count_reviews:280,
-    count_photos:127
-  }
-}
-*/
+    let paramObject = p.slice(0,7);
+    paramObject.push(review);
+    paramObject.push(user);
 
-//
-// read reviews files one by one
-// put in restaurant id and user id, which ill update later
-// can i do that?
-// try putting in 10 reviews
-//
+    console.log(paramObject);
 
-// insert into "review" column first,
+    return {
+      query: 'INSERT INTO reviews (id, useful_count, funny_count, cool_count, useful_clicked, funny_clicked, cool_clicked, review, user) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);',
+      params: paramObject
+    };
+  });
 
-
-
-  // SHAPE OF REVIEWS DATA THAT I'M READING FROM
-  /*
-    restaurant_id
-    useful_count
-    funny_count
-    cool_count
-    useful_clicked
-    funny_clicked
-    cool_clicked
-      date?
-    review text?
-      count_checkin?
-    user id?
-  */
-
-  // SHAPE OF DATABASE I'M TRYING TO WRITE TO
-  /*
-    id
-      restaurant_id
-      cool_clicked
-      cool_count
-      funny_clicked
-      funny_count
-    name // restaurant name
-    ratings
-      review
-    reviewscount
-      useful_clicked
-      useful_count
-      user
-  */
-
-fs.readFile('../data/reviewsWithRating/01.csv', (err, data) => {
-  data = String(data);
-
-  let query = data.substr(0, 1000);
-  query = query.substr(0, query.lastIndexOf('\n'));
-  query = query.replace(/\n/g, '),\n(');
-  query = query.replace(/"(\d\d?\/\d\d?\/\d\d\d\d)","(.*?)",(\d),(\d\d?\d?\d?\d?\d?\d?),(\d\.?\d?)/g, '{date:\'$1\', text_review:\'$2\', count_checkin:$3, count_starRatings:$5}, {id:$4}');
-  query = 'INSERT INTO reviews (id, useful_count, funny_count, cool_count, useful_clicked, funny_clicked, cool_clicked, review, user) VALUES (' + query + ')';
-
-  console.log(query);
-
-
-  // client.execute(query)
-  //   .then((result) => {
-  //     console.log('success');
-  //   });
-
+  client.batch(query, { prepare: true })
+    .then(result => console.log('Data insert successful!'));
 
 });
